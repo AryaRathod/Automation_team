84140e86770e50f73e6782e4868eba66ccd6fc8d
91913 <91913@MSI>
2021-08-11 14:11:47 +0530

    msg second updated

---
+++ src/Basic2/VariableInfo.java
@@ -0,0 +1,33 @@
+package Basic2;
+
+
+
+public class VariableInfo {
+
+	public static void main(String[] args)  // This is my main method//
+	{   
+		
+		System.out.println("this program is created by Amol..\n");
+		System.out.println("....This is the concept of variable and its Data Type........");
+	// variable declaration//
+		 String student_name;
+			int Roll_no;
+			float marks;
+			char grade;
+	// Variable definition //
+			student_name="Arya";
+			Roll_no=11;
+			marks=75;
+			grade='A';
+			
+	//Variable Usage //
+			System.out.println("Sudent name is " + student_name);
+			System.out.println("Roll number is " + Roll_no);
+			System.out.println("marks are " + marks);
+			System.out.println("grade is  " + grade);
+			
+
+
+	}
+
+}
