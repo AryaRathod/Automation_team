e52b85e91df51699dfc0187370b67e8480f3e16b
91913 <91913@MSI>
2021-08-11 14:53:36 +0530

    Fibonacci Series of 10 number

--- src/Practice_git.java
+++ src/Practice_git.java
@@ -7,6 +7,19 @@ public class Practice_git {
 		
 		System.out.println("Welcome to GitHub...");
 		System.out.println("hi this velocity student");
+		 int maxNumber = 10, previousNumber = 0, nextNumber = 1;
+	        System.out.print("Fibonacci Series of "+maxNumber+" numbers:");
+
+	        int i=1;
+	        while(i <= maxNumber)
+	        {
+	            System.out.print(previousNumber+" ");
+	            int sum = previousNumber + nextNumber;
+	            previousNumber = nextNumber;
+	            nextNumber = sum;
+	            i++;
+	        }
+		
 	}
 
 }
