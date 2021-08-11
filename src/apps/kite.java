27bfdee4f1e2ad96494fa9c07bcc6cd2ee972b6a
91913 <91913@MSI>
2021-08-11 11:35:30 +0530

    HI THIS IS ARYARATHOD....

---
+++ src/apps/kite.java
@@ -0,0 +1,17 @@
+package apps;
+
+
+
+public class kite {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		  System.out.println("This Automation program Control by APR......");
+		  System.out.println("Please check messanger.....");
+		  System.out.println("1234567890");
+		  System.out.println("123456789012345678901234567890");
+		
+
+}
+}
